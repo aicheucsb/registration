4e76a1782ec6cc3fde0fbec7b2328487d93f62ff
steph-anu <39209920+steph-anu@users.noreply.github.com>
2021-12-23 21:32:11 -0800

    getdata

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let label=['Start time', 'End time', 'Project name', 'Purpose','Contact person', 'Email'];
+  let labelfix = label.map(name => name.replace(' ',''));
+
+  let labels = []; //https://stackoverflow.com/questions/2383484/how-to-create-a-dynamic-object-in-a-loop
+  for (var i = 0; i < label.length; i++) {
+    labels.push({label:label[i], labelfix:labelfix[i]});
+  }
+
+  function getInputs(e) {
+    e.preventDefault();
+    let inputs = {};
+    labelfix.forEach(id => {
+      inputs[id] = document.getElementById(id).value;
+    })
+    // console.log(inputs);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+
+        <form id="reserve" onSubmit={getInputs}> 
+          {labels.map((labels) => { //https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
+            return <label for={labels.labelfix}> {labels.label}
+              <input id={labels.labelfix} name={labels.labelfix}/>
+            </label>
+          })}
+          <input type="submit" value="Submit!" />
+        </form>
+        
+      </div>
     </div>
   );
+
+  
 }
 
 export default App;
