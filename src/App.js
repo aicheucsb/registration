ddc44f02090ee7adb734be7e72ade4fb7c05811c
steph-anu <39209920+steph-anu@users.noreply.github.com>
2021-12-25 03:30:55 -0800

    styling

--- src/App.js
+++ src/App.js
@@ -28,27 +28,36 @@ function App() {
 
   return (
     <div className="App">
+      <ReactEmbeddedGoogleCalendar publicUrl = "https://calendar.google.com/calendar/embed?src=cscae19m9abei8bv23e1queim8%40group.calendar.google.com&ctz=America%2FLos_Angeles" height="500px"/>
       <div id="reservations">
-        
+        {/* <h1 id="reserve-head">Make a reservation</h1> */}
         <form id="reserve" onSubmit={getInputs}> 
           {labels.map((labels) => { //https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
             return <label for={labels.labelfix}> {labels.label}
-              <input id={labels.labelfix} name={labels.labelfix}/>
+              <div class="inputwrap">
+                <input id={labels.labelfix} name={labels.labelfix}/>
+              </div>
             </label>
           })}
           <label title="mm/dd/yyyy">Day 
-            <input type="date" id="Day" name="Day"/>
+            <div class="inputwrap">
+              <input type="date" id="Day" name="Day"/>
+            </div>
           </label>
-          <label title="format: hh:mm AM/PM">Start time
-            <input type="time" id="Starttime" name="Starttime"/>
+          <label>Start time <span title="format: hh:mm AM/PM">(?)</span>
+            <div class="inputwrap">
+              <input type="time" id="Starttime" name="Starttime"/>
+            </div>
           </label>
-          <label title="format: hh:mm AM/PM">End time 
-            <input type="time" id="Endtime" name="Endtime"/>
+          <label>End time <span title="format: hh:mm AM/PM">(?)</span>
+            <div class="inputwrap">
+              <input type="time" id="Endtime" name="Endtime"/>
+            </div>
           </label>
 
-          <input type="submit" id="submitb" value="Submit!" />
+          <input type="submit" id="submitb" value="Reserve now!" />
         </form>
-        <ReactEmbeddedGoogleCalendar publicUrl = "https://calendar.google.com/calendar/embed?src=cscae19m9abei8bv23e1queim8%40group.calendar.google.com&ctz=America%2FLos_Angeles" height="500px"/>
+        
       </div>
     </div>
   );
