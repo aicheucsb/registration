91d4cb5feacd75c29d6559c8baee6170194a9ee3
steph-anu <39209920+steph-anu@users.noreply.github.com>
2021-12-30 00:02:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [status, setStatus] = useState("");
   const [message, setMessage] = useState("");
 
-  let label=['Project name', 'Purpose','Contact person', 'Email'];
+  let label=['Project name', 'Purpose','Contact name', 'Email'];
   let labelfix = label.map(name => name.replace(' ',''));
 
   let labels = []; //https://stackoverflow.com/questions/2383484/how-to-create-a-dynamic-object-in-a-loop
@@ -58,7 +58,6 @@ function App() {
     <div className="App">
       <ReactEmbeddedGoogleCalendar publicUrl = "https://calendar.google.com/calendar/embed?src=cscae19m9abei8bv23e1queim8%40group.calendar.google.com&ctz=America%2FLos_Angeles" height="500px"/>
       <div id="reservations">
-        {/* <h1 id="reserve-head">Make a reservation</h1> */}
         <form id="reserve" onSubmit={getInputs}> 
           {labels.map((labels) => { //https://stackoverflow.com/questions/36683770/how-to-get-the-value-of-an-input-field-using-reactjs
             return <label for={labels.labelfix}> {labels.label}
